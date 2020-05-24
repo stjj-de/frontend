75efe8a4767433582322b2717be1c4f3069293c6
Moritz Ruth <dev@moritz-ruth.de>
2020-05-24 22:54:09 +0200

    Increase font-size of gottesdienst-notice

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 import { isLoggedIn } from "@/assets/js/isLoggedIn";
 
+// eslint-disable-next-line func-names
 export default function(context) {
   if (!isLoggedIn(context.app)) {
     context.redirect("/login?to=" + encodeURI(context.route.path));
