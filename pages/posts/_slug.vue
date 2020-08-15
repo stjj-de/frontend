b442d0a9ac45d70fd6aa7a2f87c39efdeeb3f1c2
Moritz Ruth <dev@moritzruth.de>
2020-08-16 00:18:19 +0200

    Show warning on irrelevant posts

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -3,6 +3,7 @@
     <NavigationBar title="Aktuelles"/>
     <main class="content">
       <div class="post-page__meta">
+        <span v-if="irrelevant" class="post-page__irrelevant">Dieser Artikel ist <b>nicht mehr relevant</b>.</span>
         <span class="post-page__title">{{ post.title }}</span>
         <span class="post-page__separator"></span>
         <span class="post-page__date">{{ formattedPublicationDate }}</span>
@@ -22,6 +23,17 @@
 <style scoped lang="scss">
   @use "~@/assets/styles/colors";
   @use "~@/assets/styles/screenSize";
+
+  .post-page__irrelevant {
+    display: block;
+    font-size: 1.5rem;
+    text-align: center;
+    margin-bottom: 20px;
+    padding: 20px 10px;
+    color: darken(colors.$red, 10);
+    border: 2px solid transparentize(colors.$red, 0.2);
+    border-radius: 10px;
+  }
 
   .post-page__meta {
     width: 700px;
@@ -83,10 +95,10 @@
       };
     },
     data: () => ({
-      post: null
+      post: {}
     }),
     async asyncData({ error, app: { $api }, params }) {
-      const post = await $api.posts.get(`_${params.slug}`, ["title", "excerpt", "slug", "publishedAt", "content", "author"]);
+      const post = await $api.posts.get(`_${params.slug}`, ["title", "excerpt", "slug", "relevantUntil", "publishedAt", "content", "author"]);
 
       if (post === null) {
         error({ statusCode: 404, m: "Dieser Artikel existiert nicht." });
@@ -98,6 +110,9 @@
     computed: {
       formattedPublicationDate() {
         return format(new Date(this.post.publishedAt), "EEEE, d.L.yyyy", { locale: dateFnsLocale });
+      },
+      irrelevant() {
+        return this.post.relevantUntil !== null && Date.parse(this.post.relevantUntil) < Date.now()
       }
     }
   };
