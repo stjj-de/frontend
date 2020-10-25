d00e286c16a93118ecb392d391dddbcd5896e409
Moritz Ruth <dev@moritzruth.de>
2020-10-25 14:50:23 +0100

    Add page redirecting to the latest video

---
+++ pages/mediathek/latest.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script>
+  export default {
+    name: "LatestVideoPage",
+    async asyncData({ $api, redirect }) {
+      const { items } = await $api.videos.list({
+        fields: ["id"],
+        limit: 1,
+        sortBy: "publishedAt",
+        ascending: false
+      })
+
+      redirect(`/mediathek/${items[0].id}`)
+    }
+  }
+</script>
